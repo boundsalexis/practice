5e6fb249c1f954bebbc208c8dc1cc238cde179da
Alexis <lxs.bnds@gmail.com>
2020-05-30 18:53:32 -0700

    absolute bare bones, what you need to be able to send a html

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
+path = require('path');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// express.json(true);
+app.get('/', (req, res) => res.sendFile(path.join(__dirname +'/index.html')))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
