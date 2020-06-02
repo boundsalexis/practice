662fb15f1b181a7279ae2acd3d1f796104ac423b
Alexis <lxs.bnds@gmail.com>
2020-06-02 16:08:25 -0700

    db connected, api serving, most basic app ever created

--- public/script.js
+++ public/script.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
     console.log("we are locked and loaded");
+  
+    document.getElementById("tips").onsubmit=(event)=>{
+      event.preventDefault();
+      var thisForm = document.forms.tips;
+
+      // console.log(thisForm);
+      var tips = new FormData(thisForm);
+      var newTip={};
+      newTip.content = tips.get('content');
+      newTip.author = tips.get('author');
+      newTip.rating = tips.get('rating');
+      console.log(newTip)
+      axios.post('/api/new',newTip).then(res=> console.log(res));
+    }
+
   });
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,27 @@
 var express = require('express');
+var connection = require('../config/connection');
 
 var router = express.Router();
 
-router.get('/', function(req, res) {
-    res.send('ami here');
+router.get('/all', function(req, res) {
+    let dbQuery = 'SELECT * FROM tips';
+    connection.query(dbQuery, (err, result)=>{
+        if(err) throw err;
+        res.json(result)
+    })
+
+    
 });
 
-router.post('/api', function(req, res) {
-    res.send('lemme be here');
+router.post('/new', function(req, res) {
+    console.log(req.body);
+    let dbQuery = 'INSERT INTO tips (content, author, rating) VALUES (?,?,?)';
+    connection.query(dbQuery,[req.body.content, req.body.author, req.body.rating] ,(err, result)=>{
+        if (err){
+            throw err
+        }
+        console.log(result)
+    })
 });
 
 module.exports = router;
